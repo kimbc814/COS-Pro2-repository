b8101843151d5fc40ae08e7eca369c897ac8ec79
bc_01 <46893535+kimbc814@users.noreply.github.com>
2023-03-05 21:43:34 +0900

    Add files via upload

---
+++ 03/3/chapter35/35.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int fun35_1() {
+	char c1 = 'a';
+	char* s = "Hello";
+	pritnf("%c\n", c1);
+	printf("%s\n", s1);
+	return 0;
+}
+int fun35_2_1() {
+	char* s1 = "Hello";
+	printf("%c\n", s1[1]);
+	printf("%c\n", s1[4]);
+	printf("%c\n", s1[5]);
+
+	return 0;
+}
+int fun35_2_2() {
+	char* s1 = "Hello";
+	s1[0] = 'A';
+	printf("%c\n", s1[0]);
+	return 0;
+}
+int fun35_4_1() {
+	char s1[30] = "Beethoven 9th Symphony";
+	printf("%c\n", s1[10]);
+
+	return 0;
+}
